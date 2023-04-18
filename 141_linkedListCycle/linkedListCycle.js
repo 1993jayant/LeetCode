1ecf3456378fea0a72d8aef8c59b8c41fc9b28db
Jayant <1993jayant@gmail.com>
2023-04-18 12:47:40 +0530

    Runtime 57 ms  Beats 99.7%   Memory 45.6 MB   Beats 17.96%

--- 141_linkedListCycle/linkedListCycle.js
+++ 141_linkedListCycle/linkedListCycle.js
@@ -11,15 +11,30 @@
  * @return {boolean}
  */
 var hasCycle = function (head) {
+  //   let curr = head
+
+  //   if(!head || head.next === null) return false
+
+  //   while(curr) {
+  //     if(curr.val === 'asdf') return true
+
+  //     curr.val = 'asdf'
+  //     curr = curr.next
+  //   }
+
+  //   return false
+
+  // Runtime 57 ms  Beats 99.7%   Memory 45.6 MB   Beats 17.96%
   let curr = head
+  let fastPtr = curr
 
-  if (!head || head.next === null) return false
+  if (!head) return false
 
-  while (curr) {
-    if (curr.val === "asdf") return true
-
-    curr.val = "asdf"
+  while (fastPtr && fastPtr.next) {
     curr = curr.next
+    fastPtr = fastPtr.next.next
+
+    if (curr === fastPtr) return true
   }
 
   return false
