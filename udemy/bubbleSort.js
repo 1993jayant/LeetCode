5b838673e54e80435809f4766761e06f5a211b4b
Jayant <1993jayant@gmail.com>
2023-05-05 14:40:49 +0530

    optimized solution for almost sorted array input

--- udemy/bubbleSort.js
+++ udemy/bubbleSort.js
@@ -1,15 +1,22 @@
 function bubbleSort(arr) {
   for (let i = arr.length - 1; i > 0; i--) {
+    let swap = false
     for (let j = 0; j < i; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j + 1]
         arr[j + 1] = arr[j]
         arr[j] = temp
+        swap = true
       }
+    }
+
+    // if there is no swap in the last iteration, it means that the array elements are sorted, and there is no need to iterate further
+    if (!swap) {
+      break
     }
   }
 
   return arr
 }
 
-console.log(bubbleSort([3, 2, 1, -1, -5]))
+console.log(bubbleSort([9, 1, 2, 3, 4, 5]))
